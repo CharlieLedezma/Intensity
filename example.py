e1da7f814334cd2b6b0f2a9803f6342be93ac8a7
CharlieLedezma <lcledezma@gmail.com>
2024-10-28 17:45:46 -0500

    Mejorando el ejercicio

--- example.py
+++ example.py
@@ -2,8 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Modifico las coordenadas
-xpoints = np.array([0, -8])
-ypoints = np.array([3, 10])
+#xpoints = np.array([0, -8])
+#ypoints = np.array([3, 10])
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(xpoints, ypoints)
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
 plt.show()
